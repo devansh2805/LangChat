a43147ae37a6db5f466b9bb9d7bf5ce21b1c7c33
devansh2805 <devansh.ds@somaiya.edu>
2021-02-05 14:10:01 +0530

    Login Screen

---
+++ android/app/src/main/kotlin/com/example/LangChat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.LangChat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new LoginPage(),
+      theme: new ThemeData(primarySwatch: Colors.grey),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new LoginPageState();
+  }
+}
+
+class LoginPageState extends State<LoginPage> {
+  var _phoneNumber;
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.white,
+      body: new Container(
+        padding: const EdgeInsets.all(40.0),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new IntlPhoneField(
+              decoration: new InputDecoration(
+                labelText: "Mobile Number",
+                border: OutlineInputBorder(borderSide: BorderSide()),
+              ),
+              initialCountryCode: 'IN',
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
