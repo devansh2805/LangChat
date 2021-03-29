9411e92dbe9d7362f3b9ae2238ba2884fc1cf7d2
devansh2805 <devansh.ds@somaiya.edu>
2021-03-29 18:07:20 +0530

    Removed unwanted import

--- lib/screens/ChatScreen.dart
+++ lib/screens/ChatScreen.dart
@@ -1,7 +1,6 @@
 import 'package:LangChat/backend/database.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:translator/translator.dart';
 
